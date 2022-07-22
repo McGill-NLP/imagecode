dd1528106ec3755857d16f3cdbec0514e47469a9
BennoKrojer <benno.krojer@gmail.com>
2022-07-22 15:09:19 -0400

    wip

--- baselines/clip/grad_contra.py
+++ baselines/clip/grad_contra.py
@@ -1,6 +1,7 @@
 # inspired from: https://github.com/openai/CLIP/issues/83
 # https://github.com/openai/CLIP/issues/83
 from importlib import import_module
+from itertools import count
 import json
 import os
 import random
@@ -20,7 +21,7 @@ import argparse
 from functools import partial
 random.seed(10)
 torch.manual_seed(10)
-wandb.init(project='finetune-clip', settings=wandb.Settings(start_method='fork'))
+wandb.init(project='clip-grad-supervision', settings=wandb.Settings(start_method='fork'))
 
 def find_best_matches(text_features, photo_features):
     similarities = (photo_features @ text_features.T).squeeze(1)
@@ -31,7 +32,7 @@ def find_best_matches(text_features, photo_features):
 
 
 def convert_models_to_fp32(model):
-    for p in model.parameters():
+    for name, p in model.named_parameters():
         p.data = p.data.float()
         p.grad.data = p.grad.data.float()
 
@@ -40,11 +41,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--batchsize', type=int, default=36)
 parser.add_argument('--grad_accumulation', type=int, default=1)
 parser.add_argument('--lr', type=float, default=4e-6)
-parser.add_argument('--max_lr', type=float, default=4e-6)
 parser.add_argument('--vit', type=str)
+parser.add_argument('--decay', default=0.01, type=float)
 parser.add_argument('--epochs', type=int, default=30)
 parser.add_argument('--data_dir', type=str, default='../../data/')
 parser.add_argument('--imgs_path', type=str, default='/network/scratch/b/benno.krojer/dataset/games')
+parser.add_argument('--save_model', action='store_true')
 parser.add_argument("--job_id")
 
 args = parser.parse_args()
@@ -89,26 +91,36 @@ dataloader_valid = DataLoader(
 
 loss_img = nn.CrossEntropyLoss()
 loss_txt = nn.CrossEntropyLoss()
-optimizer = optim.AdamW(model.parameters(), lr=args.lr, betas=(0.9, 0.98), eps=1e-6, decay=args.decay)
-if args.cycle_scheduler:
-    scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.max_lr, steps_per_epoch=len(dataloader_train)//(args.batchsize*args.grad_accumulation), epochs = args.epochs)
-
+optimizer = optim.AdamW(model.parameters(), lr=config.lr, betas=(0.9, 0.98), eps=1e-6, weight_decay=args.decay)
 best_val = 0
 
 for i in range(args.epochs):
     save_model = False
     # EVALUATE
-    if i != 0:
+    if i > 0:
         correct = 0
-        for images, text, target, is_video in tqdm.tqdm(dataloader_valid):
-            images = images.to(DEVICE)
+        total = 0
+        for image, text, target, is_video in tqdm.tqdm(dataloader_valid):
+            image = image.to(DEVICE)
             text = text.to(DEVICE)
             target = target.to(DEVICE)
             is_video = is_video.to(DEVICE)
-            ranked_idx, sim = find_best_matches(text, images)
-            if ranked_idx == target:
-                correct += 1
-        acc = correct / len(dataloader_valid)
+
+            with torch.no_grad():
+                image_features = model.encode_image(image.flatten(0, 1)).view(*image.shape[:2], -1)
+                text_features = model.encode_text(text.squeeze(1))
+            
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+
+            similarity = (image_features @ text_features.unsqueeze(2)).squeeze()
+            prediction = similarity.argmax()
+
+            total += 1
+            correct += (prediction == target)
+        acc = correct / total
+        acc = acc.item()
+        print(f'accuracy: {acc:.4f}')
         wandb.log({'val_acc': acc})
         if acc > best_val:
             best_val = acc
@@ -117,26 +129,48 @@ for i in range(args.epochs):
             for key, val in list(vars(args).items()):
                 if 'path' not in key:
                     string += f'_{val}'
-            torch.save({
-                'epoch': i,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-            }, f"checkpoints/CONTRA_clip_best_{string.replace('/', '')}.pt")
-        print('------------------------------')
+            if args.save_model:
+                torch.save({
+                    'epoch': i,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                }, f"checkpoints/CONTRA_clip_best_{string.replace('/', '')}.pt")
 
     print(f'EPOCH: {i}')
     for step, (images, text, target, is_video) in tqdm.tqdm(enumerate(dataloader_train)):
-        images = images.to(DEVICE)
-        images = images.reshape(images.shape[0]*images.shape[1], *images.shape[2:]) 
-        text = text.to(DEVICE).squeeze(1)
+        images = images.to(DEVICE).requires_grad_()
+        text = text.to(DEVICE)
         target = target.to(DEVICE)
         is_video = is_video.to(DEVICE)
+        batchsize, image_size = images.shape[0], images.shape[-1]
 
-        logits_per_image, logits_per_text = model(images, text)
+        image_features_ = model.encode_image(images.flatten(0, 1)).reshape(*images.shape[:2], -1)
+        text_features_ = model.encode_text(text.squeeze(1))
+        image_features = image_features_ / image_features_.norm(dim=-1, keepdim=True)
+        text_features = text_features_ / text_features_.norm(dim=-1, keepdim=True)
+	
+        similarity = (image_features @ text_features.unsqueeze(2)).squeeze() * model.logit_scale.exp()
+
+        zero = torch.zeros((batchsize,1,3,image_size,image_size)).cuda()
+        diff = images - torch.cat([zero, images[:,:-1,:,:,:]], dim=1)
+        diff = diff - torch.cat([images[:,1:,:,:,:], zero], dim=1)
+
+        # target_sim = similarity.gather(1, target.unsqueeze(1))
+        similarity.sum().backward(retain_graph=True)
+        img_grad = images.grad
+
+        optimizer.zero_grad() #make sure that we don't update weights based on the previous backward step
+        diff = diff.reshape(diff.shape[0]*diff.shape[1], diff.shape[2]*diff.shape[3]*diff.shape[4])
+        img_grad = img_grad.reshape(img_grad.shape[0]*img_grad.shape[1], img_grad.shape[2]*img_grad.shape[3]*img_grad.shape[4])
+        counterfactual_loss = 1 - torch.nn.functional.cosine_similarity(diff, img_grad, dim=1) #do abs() before maybe?
+        counterfactual_loss = counterfactual_loss.reshape(batchsize, 10) * is_video.unsqueeze(1)
+        counterfactual_loss = counterfactual_loss.mean()
+        
         ground_truth = torch.tensor(target).long()  # the index of the correct one
-        loss = loss_txt(logits_per_text, ground_truth)
+        loss = loss_txt(similarity, ground_truth) + args.loss_factor * counterfactual_loss
         loss.backward()
         if step % args.grad_accumulation == 0:
+            print(loss.item())
             wandb.log({'loss': loss})
             if DEVICE == "cpu":
                 optimizer.step()
@@ -145,5 +179,3 @@ for i in range(args.epochs):
                 optimizer.step()
                 clip.model.convert_weights(model)
             optimizer.zero_grad()
-            if args.cycle_scheduler:
-                scheduler.step()
