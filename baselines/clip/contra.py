14367ef69500a0d80e67fc72e634822fd1b0ccc8
BennoKrojer <benno.krojer@gmail.com>
2022-07-22 10:10:38 -0400

    cleaner

--- baselines/clip/contra.py
+++ baselines/clip/contra.py
@@ -20,7 +20,7 @@ import argparse
 from functools import partial
 random.seed(10)
 torch.manual_seed(10)
-wandb.init(project='finetune-clip', settings=wandb.Settings(start_method='fork'))
+wandb.init(project='clip-ViT-L14-336px-T5', settings=wandb.Settings(start_method='fork'))
 
 def find_best_matches(text_features, photo_features):
     similarities = (photo_features @ text_features.T).squeeze(1)
@@ -31,7 +31,7 @@ def find_best_matches(text_features, photo_features):
 
 
 def convert_models_to_fp32(model):
-    for p in model.parameters():
+    for name, p in model.named_parameters():
         p.data = p.data.float()
         p.grad.data = p.grad.data.float()
 
@@ -41,9 +41,11 @@ parser.add_argument('--batchsize', type=int, default=36)
 parser.add_argument('--grad_accumulation', type=int, default=1)
 parser.add_argument('--lr', type=float, default=4e-6)
 parser.add_argument('--vit', type=str)
+parser.add_argument('--decay', default=0.01, type=float)
 parser.add_argument('--epochs', type=int, default=30)
 parser.add_argument('--data_dir', type=str, default='../../data/')
 parser.add_argument('--imgs_path', type=str, default='/network/scratch/b/benno.krojer/dataset/games')
+parser.add_argument('--save_model', action='store_true')
 parser.add_argument("--job_id")
 
 args = parser.parse_args()
@@ -88,23 +90,36 @@ dataloader_valid = DataLoader(
 
 loss_img = nn.CrossEntropyLoss()
 loss_txt = nn.CrossEntropyLoss()
-optimizer = optim.AdamW(model.parameters(), lr=config.lr, betas=(0.9, 0.98), eps=1e-6)
+optimizer = optim.AdamW(model.parameters(), lr=config.lr, betas=(0.9, 0.98), eps=1e-6, weight_decay=args.decay)
 best_val = 0
 
 for i in range(args.epochs):
     save_model = False
     # EVALUATE
-    if i != 0:
+    if i > 0:
         correct = 0
-        for images, text, target, is_video in tqdm.tqdm(dataloader_valid):
-            images = images.to(DEVICE)
+        total = 0
+        for image, text, target, is_video in tqdm.tqdm(dataloader_valid):
+            image = image.to(DEVICE)
             text = text.to(DEVICE)
             target = target.to(DEVICE)
             is_video = is_video.to(DEVICE)
-            ranked_idx, sim = find_best_matches(text, images)
-            if ranked_idx == target:
-                correct += 1
-        acc = correct / len(dataloader_valid)
+
+            with torch.no_grad():
+                image_features = model.encode_image(image.flatten(0, 1)).view(*image.shape[:2], -1)
+                text_features = model.encode_text(text.squeeze(1))
+            
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+
+            similarity = (image_features @ text_features.unsqueeze(2)).squeeze()
+            prediction = similarity.argmax()
+
+            total += 1
+            correct += (prediction == target)
+        acc = correct / total
+        acc = acc.item()
+        print(f'accuracy: {acc:.4f}')
         wandb.log({'val_acc': acc})
         if acc > best_val:
             best_val = acc
@@ -113,26 +128,31 @@ for i in range(args.epochs):
             for key, val in list(vars(args).items()):
                 if 'path' not in key:
                     string += f'_{val}'
-            torch.save({
-                'epoch': i,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-            }, f"checkpoints/CONTRA_clip_best_{string.replace('/', '')}.pt")
-        print('------------------------------')
+            if args.save_model:
+                torch.save({
+                    'epoch': i,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                }, f"checkpoints/CONTRA_clip_best_{string.replace('/', '')}.pt")
 
     print(f'EPOCH: {i}')
     for step, (images, text, target, is_video) in tqdm.tqdm(enumerate(dataloader_train)):
         images = images.to(DEVICE)
-        images = images.reshape(images.shape[0]*images.shape[1], *images.shape[2:]) 
-        text = text.to(DEVICE).squeeze(1)
+        text = text.to(DEVICE)
         target = target.to(DEVICE)
         is_video = is_video.to(DEVICE)
 
-        logits_per_image, logits_per_text = model(images, text)
+        image_features_ = model.encode_image(images.flatten(0, 1)).reshape(*images.shape[:2], -1)
+        text_features_ = model.encode_text(text.squeeze(1))
+        image_features = image_features_ / image_features_.norm(dim=-1, keepdim=True)
+        text_features = text_features_ / text_features_.norm(dim=-1, keepdim=True)
+
+        similarity = (image_features @ text_features.unsqueeze(2)).squeeze() * model.logit_scale.exp()
         ground_truth = torch.tensor(target).long()  # the index of the correct one
-        loss = loss_txt(logits_per_text, ground_truth)
+        loss = loss_txt(similarity, ground_truth)
         loss.backward()
         if step % args.grad_accumulation == 0:
+            print(loss.item())
             wandb.log({'loss': loss})
             if DEVICE == "cpu":
                 optimizer.step()
